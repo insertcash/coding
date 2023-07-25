be5df7a5db6bb14f774da92dccca7c1203be932c
insertcash <133251298+insertcash@users.noreply.github.com>
2023-07-25 18:29:35 -0500

    commiting and pushing becasue for SOME REASON everytime i press ctrl z all my code disappears from now to the last time i COMITTED so i'm just going to commit now :|
    Signed-off-by: insertcash <133251298+insertcash@users.noreply.github.com>

--- Python/Projects/Grading/gradingSystem.py
+++ Python/Projects/Grading/gradingSystem.py
@@ -3,7 +3,6 @@ from time import sleep
 
 # todo: create a dictionary to save and retrieve from the text file with student name as key and student id as value.
 # todo: take student id values as keys in dictionary and store student line number as value
-# todo: create dictionary studentid[course] to record grade per course
 
 # Create a grading system for BSC computer science. The program should be able to capture the names, admission number
 # and the student marks for the various course units they are taking. In addition to capturing the marks, the program
@@ -50,30 +49,26 @@ if not correctuser or not correctpass:
 else:
     pass
 
-# dictionary of student names
-with open("grades.txt", "a+", encoding ="utf-8") as f:
-    f.seek(0)
-    studentdict = f.readline(len(f.readline()) - 2)
-    f.readline(2)
-    coursedict = f.readline(len(f.readline()) - 2)
-    f.readline(2)
-    gradedict = f.readline(len(f.readline()) - 2)
-
 # menu
-# note: noi stands for name or id
+# note: noi stands for name or id. ic stands for id and course
 while cont:
     menuselect = str(input("Choose number or letter:\n1. (A)dd grades\n2. (C)hange grades\n3. (R)emove grades\n4. (E)xit").replace(" ", "").lower())
     if menuselect == "a" or menuselect == "1":
         # ask for student name and id, ask for course, add grade for course, save to file, ask to cont
         name = input("Student name: ")
-        id = int(input("Student ID: "))
+        id = input("Student ID: ")
         course = input("Student course: ")
         grade = input("Student grade: ")
 
-        studentdict = studentdict | {name[id]}
-        coursedict = coursedict | {id[course]}
-        courseid = course + id
-        gradedict = gradedict | {courseid[grade]}
+        ic = id + "," + course
+
+
+        with open("grades.txt", "a+", encoding ="utf-8") as f:
+            f.write(f"{name},{id},{course},{grade}\n")
+            f.seek(0)
+            line = f.readline()
+            f.write("{" + line + "," + str(ic[grade]) + "}")
+
 
     elif menuselect == "c" or menuselect == "2":
         # ask for student name or id, ask for course, find student (https://bit.ly/3Qfo0A9), edit grade for course,
