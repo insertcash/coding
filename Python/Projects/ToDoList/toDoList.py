40531acbc9167736f5c940297771e09abacd8fd0
best <thatboii@altmails.com>
2023-10-30 18:13:20 -0700

    finished adding add functionality.

--- Python/Projects/ToDoList/toDoList.py
+++ Python/Projects/ToDoList/toDoList.py
@@ -1,64 +1,50 @@
 # to do list
 
-# imports
-import random as r
-
 # functions
 def usrinput(usrinput, type):
-    """Supports strings and intergers. If string is selected, places input into str(input(usrinput)), while removing
-     whitespaces and lowercasing letters. If interger is selected, returns int(input(usrinput)), while removing
-     whitespaces."""
+    """Supports strings and intergers. If string is selected, places input into str(input(usrinput)), while stripping
+     whitespaces and lowercasing letters. If integer is selected, returns int(input(usrinput))."""
     if type == "str":
-        return str(input(usrinput)).replace(" ", "").lower()
+        return str(input(usrinput)).strip().lower()
     elif type == "int":
-        return int(input(usrinput)).replace(" ", "")
+        return int(input(usrinput))
 
-# show list
-# read list from file
-with open("todo.txt", "r") as f:
-    todo = f.readlines()
 
 # mainloop
 run = True
 while run:
-    # display list
-    if not todo:
-        count = 0
-        items = []
-        for i in todo:
-            count += 1
-            item = i.split("`")
-            items.append(f"{count}. {item[0]} - urgency {item[1]}")
-            print(f"{count}. {item[0]} - urgency {item[1]}")
-    else:
-        print("Congrats! You have no items in your todo list!")
+    # read list from file
+    with open("todo.txt", "r") as f:
+        todo = f.readlines()
 
-    # show options
-    menu = usrinput("Options:\n1. Add item(s)\n2. Complete item(s)\n3. Change an item\n4. Exit\nSelect by number: ", "str")
-    if menu == "1":
-        bulk = usrinput("Add in bulk?(y/n): ", "str")
-        if bulk == "y":
-            pass
-            # TODO: ensure to finish the bulk functionality for adding items
-        elif bulk == "n":
-            item = usrinput("Item: ", "str")
-            urgency = usrinput("Urgency(number 1-5, 1 is most urgent): ", "int")
-            with open("todo.txt", "a") as f:
-                f.write(f"{item}`{urgency}\n")
-            print("Item added successfully!")
+    # display list
+    count = 0
+    for i in todo:
+        # reads and splits the item on the to do list. then, prints the item, and ups the count.
+        item = todo[count]
+        item = item.split("`")
+        if count == len(todo) - 1:
+            print(f"{count + 1}. {item[0]} - urgency {item[1]}")
         else:
-            print("Invalid command.")
-            continue
-    if menu == "2":
-        bulk = usrinput("Add in bulk?(y/n): ", "str")
-        if bulk == "y":
-            pass
-            # TODO: ensure to finish the bulk functionality for removing items
-        elif bulk == "n":
-            line = usrinput("Item number: ", "int") - 1
-            with open("todo.txt", "r") as f:
-                lines = f.readlines()
-            lines.remove(items[line])
-            with open("todo.txt", "w") as f:
-                f.truncate()
-                f.writelines(line)
+            print(f"{count + 1}. {item[0]} - urgency {item[1]}", end="")
+        count += 1
+        if count == len(todo):
+            break
+
+    menu = usrinput("Options:\n1. Add item\n2. Complete item\n3. Edit item\n4. Exit\nSelect by number: ", "int")
+    if menu == 1:
+        thing = usrinput("Item: ", "str")
+        urgency = usrinput("Urgency (type 1-5): ", "str")
+        saved = "`".join([thing, urgency])
+        todo.append(saved + "\n")
+        with open("todo.txt", "w") as f:
+            f.truncate(0)
+            f.writelines(todo)
+    elif menu == 2:
+        pass
+    elif menu == 3:
+        pass
+    elif menu == 4:
+        print("Exiting...")
+        run = False
+
