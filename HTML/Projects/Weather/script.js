235f4bf0e152f8e6713eda6143f8d9eab0e8828b
best <thatboii@altmails.com>
2024-06-30 14:47:34 -0500

    random push and commit
    Signed-off-by: best <thatboii@altmails.com>

--- HTML/Projects/Weather/script.js
+++ HTML/Projects/Weather/script.js
@@ -1,9 +1,32 @@
-document.addEventListener("click", function (event) {
-  // Checking if the button was clicked
-  if (!event.target.matches("#submit")) return;
+// magic stuff goes here (workin on it)
+//quick log test
+console.log("log1");
+let data
+// acquire data from api, then convert to json for actually usable stuff
+document.getElementById("loc-submit").addEventListener('click', function(){
+    // getting location to fetch data for, and combining it with api to create link
+    let fetchstr = "https://api.weatherapi.com/v1/current.json?key=57cc7adb947d4927a7623134241805&aqi=no&q="+String(document.getElementById("loc-input").value);
+    // using link in a GET request, then turning it into json, then logging to console (need to remove that)
+    let data = fetch(fetchstr)
+        .then(result => result.json())
+        .then(result => console.log(result));
 
-    let locinput = document.getElementById("loc-input").value;
-  fetch("<https://api.weatherapi.com/v1/current.json?key=da3f180c41a84bdcb30230004242104&q=98354&aqi=yes>")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+    // quick log test
+    console.log("log2");
 });
+
+// quick log test
+console.log("log3");
+
+let ins_loc_name = data.location.name;
+let ins_loc_region = data.location.region;
+let ins_loc_country = data.location.country;
+let ins_condition = data.current.condition.text;
+let ins_condition_icon = data.current.condition.icon;
+
+// jargon that actually puts the data on the page (i need to fix this)
+document.getElementById("loc-name").innerHTML = data.location.name;
+document.getElementById("loc-region").innerHTML = data.location.region;
+document.getElementById("loc-country").innerHTML = data.location.country;
+document.getElementById("condition").innerHTML = data.current.condition.text;
+document.getElementById("condition-icon").innerHTML = data.current.condition.icon;
