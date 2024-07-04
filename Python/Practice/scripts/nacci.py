ada3ee8f2dd1a15de12275a26fcdacb6e6cb754e
insertcash <133251298+insertcash@users.noreply.github.com>
2024-07-03 19:29:18 -0500

    fibbonacci edits
    Signed-off-by: best <thatboii@altmails.com>

--- Python/Practice/scripts/nacci.py
+++ Python/Practice/scripts/nacci.py
@@ -1,15 +1,22 @@
 import time as t
+import sys
+
+# FOR THE LOVE OF PERFECTLY RUNNING THE CODE FIRST TRY
+# RUN THIS CODE IN DEBUG IF YOU TRY TO REACH THE MAXIMUM INTEGER
+# YOU MUST BE ABLE TO PAUSE OR BE ABLE TO TOLERATE YOUR COMPTER TURNING INTO A JET ENGINE
+
+sys.set_int_max_str_digits(2147483647)
 
 
 def fibbonaci():
-    a = 0
-    b = 1
-    count = 0
-    while True:
-        count += 1
-        a, b = b, a + b
-        print(f"{count}: {b}")
-        t.sleep(0.25)
+	a = 0
+	b = 1
+	count = 0
+	while True:
+		t.sleep(0)
+		count += 1
+		a, b = b, a + b
+		return f"{count}: {b}"
 
 
 fibbonaci()
